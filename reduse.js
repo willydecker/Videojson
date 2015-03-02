5743f425243e68f37d3834de5bd7dbaec2151501
willydecker <wdw1062@saintjoe.edu>
2015-03-02 11:15:50 -0500

    fixed file to work with the oldest game reduce

--- reduse.js
+++ reduse.js
@@ -1,8 +1,11 @@
 var data = require('./ancestry.js');
 var ancestry = JSON.parse(data);
+//this should work. it should go one by one through each of my pieces of ancestry and return a number for all the games that have "adventure" as a genre
+//according to
+//https://msdn.microsoft.com/en-us/library/ie/ff679975%28v=vs.94%29.aspx
+console.log(JSON.stringify(ancestry.reduce(function(newest, game) {
+	return (game.YearofRelease > newest.YearofRelease)? game : newest;
+})));
 
-console.log(ancestry.reduce(function(Genre, games) {
-	return (games.Genre == 'Adventure')? games : 'none';
-}));
 		
 
